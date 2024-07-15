66970d4f62c46ba3585af13733c5bbc230f1f099
saksham011023 <msaksham1016@gmail.com>
2024-07-16 01:48:23 +0530

    Data Transformation done.

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-from src.logger import logging
+from logger import logging
 def error_message_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
