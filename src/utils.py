7bded2965042638df4f4a4bfb53717069f7a12ae
saksham011023 <msaksham1016@gmail.com>
2024-07-16 23:37:18 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,11 @@ import pandas as pd
 import dill
 import os
 import sys
+import pickle
 from sklearn.metrics import r2_score 
 from sklearn.model_selection import GridSearchCV
 
-from exception import CustomException
+from src.exception import CustomException
 
 def save_object(file_path,obj):
     try:
@@ -45,5 +46,13 @@ def evaluate_model(X_train,Y_train,X_test,Y_test,models,param):
             report[list(models.keys())[i]]=test_model_score
             
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
